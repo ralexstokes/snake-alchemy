7b36a88084928bbc82a98a921d75a21b6962c2a8
Alex Stokes <r.alex.stokes@gmail.com>
2018-05-04 18:41:44 -0700

    clean up some code, fix typo, use latest fn name

--- src/snake_alchemy/core.clj
+++ src/snake_alchemy/core.clj
@@ -1,18 +1,15 @@
 (ns snake-alchemy.core
   (:gen-class)
-  (:require [snake-alchemy.transform :as transform]
+  (:require [snake-alchemy.vyper :as vyper]
             [clojure.java.io :as io]
             [clojure.pprint :as pp]))
 
-(defn- parse [source-str]
-  (read-string source-str))
-
-(def load (comp parse slurp io/resource))
+(def load (comp read-string slurp io/resource))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (-> (first args)
       load
-      transform/serpent->vyper
+      vyper/from-serpent
       pp/pprint))
